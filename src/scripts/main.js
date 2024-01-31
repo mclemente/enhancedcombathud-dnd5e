ead349d5f6d841ae1490060d2c2f1c7f59b8d167
Matheus Clemente <matheus.xb@gmail.com>
2024-01-31 03:14:12 -0300

    Change indentation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,9 @@
-import {initConfig} from "./config.js";
+import { initConfig } from "./config.js";
 import { registerSettings } from "./settings.js";
 
 export const MODULE_ID = "enhancedcombathud-dnd5e";
 
 Hooks.on("setup", () => {
-    registerSettings();
-    initConfig();
+	registerSettings();
+	initConfig();
 });
